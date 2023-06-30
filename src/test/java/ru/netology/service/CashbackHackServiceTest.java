dcf81186416f258755f74da5d30f9f864f687e04
Yackov <yackov.irkyt@gmail.com>
2023-06-30 18:26:05 +0500

    Add test junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,32 @@
 package ru.netology.service;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+    @Test
+    public void positiveCashbackService() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void zeroCashbackService() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void negativeCashbackService() {
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 }
